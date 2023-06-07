1c44d194a8c1103778b74db7678bf8f897866958
Shukhratbek Mamadaliev <shukhratbek_mamadaliev@epam.com>
2023-06-08 00:51:30 +0500

    refactor: remove parameters

--- src/main/java/org/itpu/fopjava_course_work/parameter/Parameter.java
+++
@@ -1,7 +0,0 @@
-package org.itpu.fopjava_course_work.parameter;
-
-import org.itpu.fopjava_course_work.entity.Appliance;
-
-public interface Parameter<A extends Appliance<A>> {
-    boolean test(A appliance);
-}
